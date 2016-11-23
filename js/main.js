37ae69ffbb58702caceffce0c0c14ac42814197c
someoneontheinternet <henryjiang0x3639@gmail.com>
2016-11-23 07:46:10 -0800

    Commit
    Added the main menu and parallax effect.

---
+++ js/main.js
@@ -0,0 +1,44 @@
+﻿
+// Test
+var windowWidth;
+var windowHeight;
+
+$(document).ready(function () {
+
+    windowResize();
+
+    $('html, body').animate({
+        scrollTop: 0
+    }, 1);
+
+    hideLoader();
+
+    windowWidth = $(window).width();
+    windowHeight = $(window).height();
+
+    console.log("Window loaded w/h is: " + windowWidth + " " + windowHeight);
+
+    $(window).resize(windowResize);
+
+});
+
+function windowResize() {
+    windowWidth = $(window).width();
+    windowHeight = $(window).height();
+    
+    // === Change Parallax to viewport width and height
+
+    $('.parallax-wrapper').css('width', windowWidth + "px");
+    $('.parallax-wrapper').css('height', windowHeight + "px");
+       
+}
+
+function hideLoader() {
+    $('#loader').fadeOut(500);
+    $('.section-left').delay(500).hide("slide", { direction: "left" }, 500);
+    $('.section-right').delay(500).hide("slide", { direction: "right" }, 500);
+
+    setTimeout(function () {
+        $('#loader-wrapper').remove();
+    }, 1500);
+}
